c189523efcc0b5cb45a016e906d9828eac85e382
aleksandragr <grujica995@gmail.com>
2018-12-10 19:23:22 +0100

    second commit

---
+++ NaucnaCentrala2018/src/main/java/naucnaCentrala/model/User.java
@@ -0,0 +1,126 @@
+package naucnaCentrala.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String surname;
+	private String city;
+	private String country;
+	private String email;
+	private boolean isAuthor;
+	
+	private String username;
+	private String password;
+	
+	public User() {
+		
+		
+		
+	}
+	
+
+	public User(String name, String surname, String city, String country, String email, boolean isAuthor,
+			String username, String password) {
+		
+		this.name = name;
+		this.surname = surname;
+		this.city = city;
+		this.country = country;
+		this.email = email;
+		this.isAuthor = true;
+		this.username = username;
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public boolean isAuthor() {
+		return isAuthor;
+	}
+
+
+	public void setAuthor(boolean isAuthor) {
+		this.isAuthor = isAuthor;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
---
+++ NaucnaCentrala2018/src/main/java/naucnaCentrala/model/Work.java
@@ -0,0 +1,84 @@
+package naucnaCentrala.model;
+
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import naucnaCentrala.model.User;
+
+@Entity
+public class Work {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String heading;
+	private ArrayList<User> coauthors;
+	private ArrayList<String> keyTerms;
+	private boolean isAbstract;
+	private String section;
+	//PDF dokument
+	//konacna verzija rada
+	
+	public Work() {
+		
+		
+		
+	}
+
+	public Work(String heading, ArrayList<User> coauthors, ArrayList<String> keyTerms, boolean isAbstract,
+			String section) {
+		
+		this.heading = heading;
+		this.coauthors = coauthors;
+		this.keyTerms = keyTerms;
+		this.isAbstract = isAbstract;
+		this.section = section;
+	}
+
+	public String getHeading() {
+		return heading;
+	}
+
+	public void setHeading(String heading) {
+		this.heading = heading;
+	}
+
+	public ArrayList<User> getCoauthors() {
+		return coauthors;
+	}
+
+	public void setCoauthors(ArrayList<User> coauthors) {
+		this.coauthors = coauthors;
+	}
+
+	public ArrayList<String> getKeyTerms() {
+		return keyTerms;
+	}
+
+	public void setKeyTerms(ArrayList<String> keyTerms) {
+		this.keyTerms = keyTerms;
+	}
+
+	public boolean isAbstract() {
+		return isAbstract;
+	}
+
+	public void setAbstract(boolean isAbstract) {
+		this.isAbstract = isAbstract;
+	}
+
+	public String getSection() {
+		return section;
+	}
+
+	public void setSection(String section) {
+		this.section = section;
+	}
+	
+
+}
